3d5f3a328ff76fa39bae0582ad4ef9dd72d15866
DivyanshK84 <divyanshk@lambdatest.com>
2025-02-28 12:43:47 +0530

    Initial Commit

---
+++ smartInvertCheck/smartInvertCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  smartInvertCheckApp.swift
+//  smartInvertCheck
+//
+//  Created by Divyansh Kaushik on 28/02/25.
+//
+
+import SwiftUI
+
+@main
+struct smartInvertCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
